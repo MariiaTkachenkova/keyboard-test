9affe583e035d71626c111da81508706015bd293
Maria Tkachenkova <mashynja1795@gmail.com>
2023-08-26 18:45:03 +0400

    Initial Commit

---
+++ KeyboardTest/KeyboardTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyboardTestApp.swift
+//  KeyboardTest
+//
+//  Created by Maria Tkachenkova on 26.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct KeyboardTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
